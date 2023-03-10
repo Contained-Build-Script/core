895d97ecd7c0527c9f3c8a2ec95cc5ff70c75635
SMJSGaming <sjoerd.t@live.nl>
2023-03-10 19:49:51 +0100

    Added an abstract token for custom static tokens

---
+++ src/tokenizer/AbstractToken.ts
@@ -0,0 +1,22 @@
+import type { TrimmedDataReader } from "../utils/TrimmedDataReader";
+import { TokenType } from "../enums/TokenType";
+import { Token } from "../templates/Token";
+
+export class AbstractToken<T extends TokenType> extends Token<T, string> {
+
+    private readonly token: string;
+
+    constructor(token: string, type: T extends TokenType.VALUE ? never : T, data: TrimmedDataReader) {
+        super(type, data);
+
+        this.token = token;
+    }
+
+    protected parse(data: TrimmedDataReader): string | void {
+        const result = data.read(this.token);
+
+        if (result) {
+            return result;
+        }
+    }
+}
