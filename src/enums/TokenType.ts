5a10fd6c6944be55d0221a1cb831a635c1e81442
SMJSGaming <sjoerd.t@live.nl>
2023-03-10 19:25:47 +0100

    Added a new token type and documentations

--- src/enums/TokenType.ts
+++ src/enums/TokenType.ts
@@ -1,7 +1,14 @@
 export enum TokenType {
+    // A value like a string, number, boolean, or null.
     VALUE,
+    // Additional characters that add context to a larger token like {}[]().
     CONTEXT,
+    // A keyword like if, else, while, etc.
     KEYWORD,
+    // A variable like #var, #var1, #var2, etc.
+    VARIABLE,
+    // An operator like +, -, *, /, etc.
     OPERATOR,
+    // A keyword or variable definition keyword like define, string, number, etc.
     DEFINITION
 }
