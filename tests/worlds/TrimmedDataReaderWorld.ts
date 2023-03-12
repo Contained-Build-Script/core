f21a4535929b246a2c5309cb6722683d101f8c30
SMJSGaming <sjoerd.t@live.nl>
2023-03-12 11:33:02 +0100

    Forgot to submit the world env update

--- tests/worlds/TrimmedDataReaderWorld.ts
+++ tests/worlds/TrimmedDataReaderWorld.ts
@@ -4,7 +4,7 @@ import type { TrimmedDataReader } from "../../src/utils/TrimmedDataReader";
 export interface TrimmedDataReaderWorld extends World {
     matches: Array<RegExpExecArray | string | void>;
     readers: TrimmedDataReader[];
-    checkpoints: number[];
+    checkpoints: number[][];
     table: string[];
     read: RegExp | string;
 }
