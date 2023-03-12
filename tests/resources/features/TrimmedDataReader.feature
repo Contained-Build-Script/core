fafcb507aa53521d34502a02aaff5139feb8d9e2
SMJSGaming <sjoerd.t@live.nl>
2023-03-12 01:51:37 +0100

    Optimized the tests and added checkpoint tests

--- tests/resources/features/TrimmedDataReader.feature
+++ tests/resources/features/TrimmedDataReader.feature
@@ -1,12 +1,27 @@
 Feature: Trimmed data reader test
 
-  Scenario: Parse a piece of data with a space appended
+  Scenario: Parse a piece of data with trimmable data prepended
     Given the following lines:
+      | test |
       |  test |
       | /* comment */test |
       | /* comment */ test |
-      | /* comment */\n//test\ntest |
+      | /* comment */\n //test\ntest |
     When "test" is read
     Then all matches are found
     And all offsets are at the end of the input data
+    And all checkpoints are cleaned up
+
+  Scenario: Set a checkpoint and use the checkpoint after parsing
+    Given the following lines:
+      | test |
+      |  test |
+      | /* comment */test |
+      | /* comment */ test |
+      | /* comment */\n //test\ntest |
+    When a checkpoint is set
+    And "test" is read
+    And the last set checkpoint is used
+    Then all matches are found
+    And all offsets are at the start of the input data
     And all checkpoints are cleaned up
--- tests/steps/TrimmedDataReader.test.ts
+++ tests/steps/TrimmedDataReader.test.ts
@@ -6,41 +6,54 @@ import { TrimmedDataReader } from "../../src/utils/TrimmedDataReader";
 Before<TrimmedDataReaderWorld>(function () {
     this.matches = [];
     this.readers = [];
+    this.checkpoints = [];
     this.table = [];
     this.read = "";
 });
 
 Given<TrimmedDataReaderWorld>("the following lines:", function (table: DataTable) {
-    this.table = table.rows().map((row) => row[0]);
+    this.table = table.rows().map(([ line ]) => {
+        this.readers.push(new TrimmedDataReader(line));
+
+        return line;
+    });
+});
+
+When<TrimmedDataReaderWorld>("a checkpoint is set", function () {
+    this.readers.forEach((reader) => {
+        this.checkpoints.push(reader.addCheckpoint());
+    });
 });
 
 When<TrimmedDataReaderWorld>("{string} is read", function (string: string) {
     this.read = string;
+    this.readers.forEach((reader) => {
+        this.matches.push(reader.read(string))
+    });
+});
 
-    for (const line of this.table) {
-        const reader = new TrimmedDataReader(line);
-
-        this.readers.push(reader);
-        this.matches.push(reader.read(this.read));
-    }
+When<TrimmedDataReaderWorld>("the last set checkpoint is used", function () {
+    this.readers.forEach((reader, i) => {
+        reader.revertToCheckpoint(this.checkpoints[i]);
+    });
 });
 
 Then<TrimmedDataReaderWorld>("all matches are found", function () {
     expect(this.read).to.be.a("string", "This step requires a string to be read");
 
-    for (const match of this.matches) {
+    this.matches.forEach((match) => {
         expect(match).to.be.a("string").that.equals(this.read);
-    }
+    });
 });
 
-Then<TrimmedDataReaderWorld>("all offsets are at the end of the input data", function () {
-    for (let i = 0; i < this.table.length; i++) {
-        expect(this.readers[i]["index"]).to.be.a("number").that.equals(this.table[i].length);
-    }
+Then<TrimmedDataReaderWorld>(/^all offsets are at the (start|end) of the input data$/, function (type: string) {
+    this.table.forEach((line, i) => {
+        expect(this.readers[i]["index"]).to.be.a("number").that.equals(type == "start" ? 0 : line.length);
+    });
 });
 
 Then<TrimmedDataReaderWorld>("all checkpoints are cleaned up", function () {
-    for (const reader of this.readers) {
+    this.readers.forEach((reader) => {
         expect(reader["checkpoints"]).to.be.an("array").that.is.empty;
-    }
+    });
 });
