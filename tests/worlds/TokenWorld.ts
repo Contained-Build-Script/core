a8d903f5e3cf947cfc14a10dd7f098fb5128ccd1
SMJSGaming <sjoerd.t@live.nl>
2023-04-05 19:05:09 +0200

    Fixed the token world type

--- tests/worlds/TokenWorld.ts
+++ tests/worlds/TokenWorld.ts
@@ -1,12 +1,10 @@
 import type { World } from "@cucumber/cucumber";
-import type { TokenTypeToDataType } from "../../src/lexer/types/TokenTypeToDataType";
 import type { TrimmedDataReader } from "../../src/lexer/utils/TrimmedDataReader";
 import type { AnyTokenClass } from "../../src/lexer/types/AnyTokenClass";
-import type { TokenType } from "../../src/lexer/enums/TokenType";
 import type { Token } from "../../src/lexer/Token";
 
 export interface TokenWorld extends World {
     tokens: AnyTokenClass[];
-    tokenInstances: Token<TokenType, TokenTypeToDataType<TokenType>>[];
+    tokenInstances: Token[];
     reader?: TrimmedDataReader;
 }
